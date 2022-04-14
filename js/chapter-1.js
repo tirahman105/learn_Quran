b7a47c46938d8ec063e6f2ab9817a71daf67a373
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2022-04-14 13:35:22 +0600

    chapter 1 demo created

---
+++ js/chapter-1.js
@@ -0,0 +1,13 @@
+
+
+		const alif = new Audio();
+          alif.src = "./audio/alif.mp3";
+
+          const ba = new Audio();
+          ba.src = "audio/ba.mp3";
+
+          const ta = new Audio();
+          ta.src = "audio/ta.mp3";
+
+          const ba = new Audio();
+          ba.src = "audio/ba.mp3";
